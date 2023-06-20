429d9dd05a15912c6175eeb5fca81d6c20e8e6a3
Jensone <hello@jensone.com>
2023-06-20 11:09:44 +0200

    ✨ Test et découverte de Slugify

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use Cocur\Slugify\Slugify;
 use App\Repository\ProductRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class ProductController extends AbstractController
 {
@@ -14,8 +16,28 @@ class ProductController extends AbstractController
         ProductRepository $productRepository
     ): Response
     {
+        $slugTest = new Slugify();
+        
         return $this->render('product/index.html.twig', [
-            'products' => $productRepository->findAll(),
-        ]);
+            'products' => $productRepository->findBy(
+                ['isPublished' => true],
+                ['name' => 'ASC']
+            ),
+            'slugTest' => $slugTest->slugify('Generate a slug')
+    ]);
+    }
+
+    #[Route('/{id}', name: 'product_show')]
+    public function show(
+        ProductRepository $productRepository,
+        Request $request
+    ): Response
+    {
+        $id = $request->get('id');
+        return $this->render('product/show.html.twig', [
+            'product' => $productRepository->findOneBy(
+                ['id' => $id]
+                )
+    ]);
     }
 }
